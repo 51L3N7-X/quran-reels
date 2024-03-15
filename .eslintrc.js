9bd9808ac3cc12b2131a1d6121889bd9dbd0c276
Yui <yui300127@gmail.com>
2024-03-16 00:50:24 +0300

    build: updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
   extends: ["airbnb-base", "plugin:prettier/recommended", "airbnb"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
   rules: {
+    "max-len": ["error", { comments: 120 }],
+
+    "no-console": ["off"],
+    "no-plusplus": ["off"],
+    "operator-linebreak": ["off"],
+    curly: ["off"],
+    "nonblock-statement-body-position": ["off"],
+
     "prettier/prettier": [
       "error",
       {
