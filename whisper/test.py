c087afae5367c513a4c4d2664592df6c52419201
Linus Torvalds <torvalds@linux-foundation.org>
2024-04-01 03:53:45 +0300

    Python is my favorite language 💕

--- whisper/test.py
+++ whisper/test.py
@@ -1,10 +1,14 @@
 import whisper_timestamped as whisper
+import json
 
 audio = whisper.load_audio("tt.mp3")
 
-model = whisper.load_model("large" , device="cuda")
+model = whisper.load_model("large", device="cuda")
 
 result = whisper.transcribe(model, audio, language="ar")
 
-import json
-print(json.dumps(result, indent = 2, ensure_ascii = False))
+data = json.dumps(result, indent=2, ensure_ascii=False).encode(
+    'utf-8')
+
+with open("../data/data.json", "w", encoding="utf-8") as outfile:
+    outfile.write(data)
